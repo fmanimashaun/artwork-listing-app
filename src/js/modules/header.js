b93cad3931b3e7722c04788a0317831fb1550f3c
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-08 14:24:23 +0100

    Update build

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,13 +1,17 @@
+import logoIcon from '../../img/logo.png';
+import menuIcon from '../../img/menu.svg';
+import closeIcon from '../../img/close.svg';
+
 const pageContainer = document.querySelector('.header');
 
 const header = `
       <div class="header__wrapper">
-        <h1 class='header__logo'><span>Art</span><img class='header__logo-img' src="img/logo.png"
+        <h1 class='header__logo'><span>Art</span><img class='header__logo-img' src="${logoIcon}"
             alt="art painting illustration"><span>ork</span></h1>
         <nav class="header__nav">
           <button class="header__nav-btn close">
-            <img class='btn-img btn-open' src="img/menu.svg" alt="menu button icon">
-            <img class='btn-img btn-close' src="img/close.svg" alt="close button icon">
+            <img class='btn-img btn-open' src="${menuIcon}" alt="menu button icon">
+            <img class='btn-img btn-close' src="${closeIcon}" alt="close button icon">
           </button>
           <ul class="header__nav-list close">
             <li class="header__nav-item"><a class='header__nav-link active' href='#'>Artworks</a></li>
