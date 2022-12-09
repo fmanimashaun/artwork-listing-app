e6ebea19bf0fcdfcf0d477e1263a7856856fa081
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-09 17:32:06 +0100

    Update home page display api call

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,18 @@ import removeActive from './modules/removeActive.js';
 import pageNav from './modules/pageNav.js';
 import pageRender from './modules/pageRender.js';
 import artworks from './modules/artworks.js';
+import fetchData from './modules/fetchApi.js';
 
 renderHeader();
 
 const tablist = document.querySelectorAll('.header__nav-link');
 const pageMain = document.querySelector('.main');
+const URL = 'https://api.artic.edu/api/v1/artworks?limit=20&fields=id,title,artist_display,place_of_origin,credit_line,term_titles,image_id';
+
+// add event listener to the nav tab
 pageNav(tablist, removeActive);
 
-pageRender(pageMain, artworks);
+// call data from artworks api and rendering on page;
+fetchData(URL).then((artworkArr) => {
+  pageRender(pageMain, artworks, artworkArr.data);
+});
--- src/js/modules/pageNav.js
+++ src/js/modules/pageNav.js
@@ -1,8 +1,10 @@
 import pageRender from './pageRender.js';
 import artworks from './artworks.js';
 import shop from './shop.js';
+import fetchData from './fetchApi.js';
 
 const pageMain = document.querySelector('.main');
+const URL = 'https://api.artic.edu/api/v1/artworks?limit=20&fields=id,title,artist_display,place_of_origin,credit_line,term_titles,image_id';
 
 const pageNav = (navItems, removeClass) => {
   navItems.forEach((tab) => {
@@ -10,7 +12,10 @@ const pageNav = (navItems, removeClass) => {
       if (event.target.textContent === 'Artworks') {
         removeClass(navItems);
         event.target.classList.add('active');
-        pageRender(pageMain, artworks);
+        // call data from artworks api and rendering on page;
+        fetchData(URL).then((artworkArr) => {
+          pageRender(pageMain, artworks, artworkArr.data);
+        });
       } else if (event.target.textContent === 'Exhibitions') {
         removeClass(navItems);
         event.target.classList.add('active');
