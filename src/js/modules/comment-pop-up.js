c9f65d61c0ea5c67aa238653a3c2dd9590d65321
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-10 10:31:09 +0100

    Update comment pop-up

--- src/js/modules/comment-pop-up.js
+++ src/js/modules/comment-pop-up.js
@@ -6,7 +6,6 @@ const commentPopUp = () => {
   const commentBtns = document.querySelectorAll('.comment-btn');
   commentBtns.forEach((button) => {
     button.addEventListener('click', (e) => {
-      console.log('clicked');
       const id = e.target.id.split('-')[1];
       const URL = `https://api.artic.edu/api/v1/artworks/${id}?fields=id,title,artist_display,place_of_origin,credit_line,term_titles,image_id`;
       // call data from artworks api and rendering on page;
