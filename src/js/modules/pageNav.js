ff915be093aab9868eff23d115ced8cc9f458d29
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-12 10:06:38 +0100

    Update page navigation function

--- src/js/modules/pageNav.js
+++ src/js/modules/pageNav.js
@@ -12,6 +12,7 @@ const pageNav = (navItems, removeClass) => {
       if (event.target.textContent === 'Artworks') {
         removeClass(navItems);
         event.target.classList.add('active');
+        pageMain.innerHTML = '';
         // call data from artworks api and rendering on page;
         const URL = 'https://api.artic.edu/api/v1/artworks?limit=20&fields=id,title,artist_display,place_of_origin,credit_line,term_titles,image_id';
         fetchData(URL).then((artworkArr) => {
